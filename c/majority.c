cf86a8a4d01bd3ff29fef4da907540e68bcd61ea
anga kishor <147230833+angakishor@users.noreply.github.com>
2025-01-23 10:30:06 +0530

    Update majority.c
    majority element in c

--- c/majority.c
+++ c/majority.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int n, i, j, count;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        count = 0;
+        for (j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if (count > n / 2) {
+            printf("Majority element is: %d\n", arr[i]);
+            break;
+        }
+    }
+
+    if (i == n) {
+        printf("No majority element found.\n");
+    }
+
+    return 0;
+}
